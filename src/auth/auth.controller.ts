60b39290274dfc09a289aace2de86605aa260851
ASMR DEVELOPER <141846914+asmr-developer777@users.noreply.github.com>
2023-08-13 17:28:53 +0200

    + added project files

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { User } from '../user.entity';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/register')
+  registerUser(@Body() user: User, @Res() resp: Response) {
+    return this.authService.registerUser(user, resp);
+  }
+
+  @Post('/login')
+  loginUser(@Body() user: User, @Res() res: Response) {
+    return this.authService.loginUser(user, res);
+  }
+
+  @Get('/user')
+  authUser(@Req() req: Request, @Res() resp: Response) {
+    return this.authService.authUser(req, resp);
+  }
+
+  @Post('/refresh')
+  refreshUser(@Req() req: Request, @Res() resp: Response) {
+    return this.authService.refreshUser(req, resp);
+  }
+
+  @Get('/logout')
+  logoutUser(@Res() resp: Response) {
+    return this.authService.logoutUser(resp);
+  }
+}
